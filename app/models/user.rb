7d856229313c8b72cbdeccf2776e158543b8cbb4
w33shizushizu33w <shizue_1707@yahoo.co.jp>
2021-09-07 22:33:23 +0900

    made change

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    validates :first_name, presence: true, length: { maximum: 50 }
+    validates :last_name, presence: true, length: { maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+                      format: { with: VALID_EMAIL_REGEX }, 
+                      uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, length: { minimum: 6 } 
+    validates :contuct_number, presence: false, length: { maximum: 140 }
+end
