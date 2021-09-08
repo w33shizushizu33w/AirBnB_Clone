61200be1c8566d9bd277e3e2de9becf305dba5c0
w33shizushizu33w <88189930+w33shizushizu33w@users.noreply.github.com>
2021-09-08 15:04:52 +0900

    Home_Page created (#1)
    * made change
    
    * made changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  root 'home_pages#home'
+  get 'home_pages/home'
+  get 'home_pages/about', to: 'home_pages#about'
+  get 'home_pages/contact', to: 'home_pages#contact'
+  get '/signup', to: 'users#new' 
 end
