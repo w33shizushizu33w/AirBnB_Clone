70e7035375cd6ec40c60b07cdc9e268154ad1ac0
w33shizushizu33w <88189930+w33shizushizu33w@users.noreply.github.com>
2021-09-15 16:50:29 +0900

    create listing (#3)
    * made change and got error
    
    * made chang at room.rb
    
    * made change
    
    * added albam space
    
    * made change
    
    * changed room_controller

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,23 @@
+class ImagesController < ApplicationController
+    def create
+       @room = Room.find(params[:room_id])
+       if params[:images]
+        params[:images].each do |img|
+            @room.images.create(image: img)
+       end
+
+        @images = @room.images
+        redirect_back(fallback_location: request.referer, notice: 'Images added successfully') 
+      end
+    end
+
+    def destroy
+       @images = Image.find(params[:id])
+       @room = Room.find(params[:room_id])
+       
+       if @images.destroy
+        @room.update_attribute(:is_active, false) if !@room.images.any?
+        redirect_back(fallback_location: request.referer, alert: "Image deleted...")
+       end
+    end
+end
---
+++ app/helpers/rooms_helper.rb
@@ -0,0 +1,5 @@
+module RoomsHelper
+    def is_ready_to_publish(room)
+        room.address.present? && room.summery.present? && room.address.present? && room.listing_name.present? && room.address.present? && room.price.present? && room.address.present? && room.images.present? && room.address.present? && room.home_type.present? && room.address.present? && room.room_type.present? && room.address.present? && room.accommodate.present? && room.address.present? && room.bedroom.present? && room.address.present? && room.bathroom.present? && room.address.present? && room.address.present?
+    end
+end
